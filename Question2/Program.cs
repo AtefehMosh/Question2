717b6b2b032930246d4de1d03b41bf721fbb8895
AtefehMosh <elina.ee1.8532@gmail.com>
2023-04-16 07:58:29 +0430

    Add project files.

---
+++ Question2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Clac(x);
+        }
+        private static void Clac(int x)
+        {
+            double result = (Math.Pow(x, 2)) + (x * 2) - 4;
+            Console.WriteLine("The Result is:" + result);
+        }
+
+    }
+}
